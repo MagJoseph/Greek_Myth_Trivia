ea68421559dce03c538ad63ca05a7191a71909f7
Magdalena Joseph <magdalena.joseph@outlook.com>
2022-03-08 14:00:45 -0500

    added numbers to qestions

--- script.js
+++ script.js
@@ -1,51 +1,51 @@
 const questions = [
  {
-     question: "In Greek mythology Medusa’s hair was made of what?",
+     question: "1. In Greek mythology Medusa’s hair was made of what?",
      answers: ["1. Feathers", "2. Fire", "3. Flowers", "4. Snakes"],
      correct: "4. Snakes"
  },
  {
-    question: "Which creature in Greek mythology was half-man and half-bull?",
+    question: "2. Which creature in Greek mythology was half-man and half-bull?",
     answers: ["1. Perseus", "2. Minotaur", "3. Pegasus", "4. Centaur"],
     correct: "2. Minotaur"
 },
  {
-    question: "According to legend, who fired the arrow that hit Achilles in the heel, his only vulnerable spot?",
+    question: "3. According to legend, who fired the arrow that hit Achilles in the heel, his only vulnerable spot?",
     answers: ["1. Paris", "2. Heracles", "3. Demeter", "4. Helena"],
     correct: "1. Paris"
  },
  {
-    question: "Who was the ancient Greek God was both God of the Underworld and God of the Dead?",
+    question: "4. Who was the ancient Greek God was both God of the Underworld and God of the Dead?",
     answers: ["1. Zeus", "2. Hades", "3. Ares", "4. Apollo"],
     correct: "2. Hades"
 },
  {
-    question: "What is the name of the Hero who killed the Medusa?",
+    question: "5. What is the name of the Hero who killed the Medusa?",
     answers: ["1. Jason", "2. Perseus", "3. Icarus", "4. Kind Minos"],
     correct: "2. Perseus"
  },
  {
-    question: "Who was the Greek hero assigned the twelve labors?",
+    question: "6. Who was the Greek hero assigned the twelve labors?",
     answers: ["1. Odysseus", "2. Achilles", "3. Prometheus", "4. Heracles"],
     correct: "4. Heracles"
  },
  {
-    question: "According to Greek Mythology, who is the keeper of time?",
+    question: "7. According to Greek Mythology, who is the keeper of time?",
     answers: ["1. Eros", "2. Oedipus", "3. Chronos", "4. Daedalus"],
     correct: "3. Chronos"
  },
  {
-    question: "Which creature tries to lure Odysseus from his ship with beautiful singing?",
+    question: "8. Which creature tries to lure Odysseus from his ship with beautiful singing?",
     answers: ["1. Sirens", "2. Harpies", "3. Chimeras", "4. Gorgons"],
     correct: "1. Sirens"
  },
  {
-    question: "What animal was created by Poseidon?",
+    question: "9. What animal was created by Poseidon?",
     answers: ["1. Dolphin", "2. Dog", "3. Whale", "4. Horse"],
     correct: "4. Horse"
 },
  {
-    question: "Who is the goddess of wisdom?",
+    question: "10. Who is the goddess of wisdom?",
     answers: ["1. Aphrodite", "2. Athena", "3. Hera", "4. Artemis"],
     correct: "2. Athena"
  }
